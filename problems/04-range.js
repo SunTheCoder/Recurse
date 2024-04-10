00902b50aeda60544751147b794a397a2baf8d61
SunTheCoder <sunenglishjr@gmail.com>
2024-04-09 21:03:13 -0400

    solved04

--- problems/04-range.js
+++ problems/04-range.js
@@ -5,17 +5,36 @@ the start, return an empty array.
 
 Examples:
 
-range(1, 5); // [1, 2, 3, 4]
-range(3, 4); // [3]
-range(7, 6); // []
 ***********************************************************************/
 
 
 function range(start, end) {
-  // Your code here 
+  
+    if (start >= end) {
+      return []
+    }
+
+    let numbers = range(start + 1, end)
+    console.log(numbers)
+
+    return [start, ...numbers]
+
+    // if (start >end) {
+    //   return []
+    // }
+
+    // let numbers = range(start + 1, end)
+    //  if (start < end) {
+    //   numbers.unshift(start)
+    //  }
+
+    //  return numbers
 }
 
 
+console.log(range(1, 5)); // [1, 2, 3, 4]
+console.log(range(3, 4)); // [3]
+console.log(range(7, 6)); // []
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = range;
